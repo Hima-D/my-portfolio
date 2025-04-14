40f9b824b987c7c0c1b3e01e3e9ed897e3da73b5
Hima D <70571561+Hima-D@users.noreply.github.com>
2025-04-15 02:19:23 +0530

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from 'react';
-import { FaEnvelope, FaLinkedin, FaGithub, FaCode, FaLaptopCode } from 'react-icons/fa';
+import { FaEnvelope, FaLinkedin, FaCode, FaLaptopCode } from 'react-icons/fa';
 import Header from '@/components/header';
 
 // Skill items
